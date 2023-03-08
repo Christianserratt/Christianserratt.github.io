45206d24e6d5f0611a6265986023926c84aed0a3
Christianserratt <127274464+Christianserratt@users.noreply.github.com>
2023-03-08 00:15:11 -0300

    initial commit

---
+++ Sintaxsis.js
@@ -0,0 +1,73 @@
+/**var boton1; 
+boton1=0;
+
+document.getElementById('datospersonales1').addEventListener('click', function(){
+if(boton1==0){
+    document.getElementById('divdatosp').style.display="block";
+    boton1=1;
+}
+else{
+        document.getElementById('divdatosp').style.display="none";
+        boton1=0;
+}
+})
+var boton2; 
+boton2=0;
+
+document.getElementById('formacionacademica1').addEventListener('click', function(){
+if(boton2==0){
+    document.getElementById('divformacd').style.display="block";
+    boton2=1;
+}
+else{
+        document.getElementById('divformacd').style.display="none";
+        boton2=0;
+}
+})
+var boton3; 
+boton3=0;
+
+document.getElementById('informatica1').addEventListener('click', function(){
+if(boton3==0){
+    document.getElementById('divinfo').style.display="block";
+    boton3=1;
+}
+else{
+        document.getElementById('divinfo').style.display="none";
+        boton3=0;
+}
+})
+var boton4; 
+boton4=0;
+
+document.getElementById('idiomas1').addEventListener('click', function(){
+if(boton4==0){
+    document.getElementById('dividioma').style.display="block";
+    boton4=1;
+}
+else{
+        document.getElementById('dividioma').style.display="none";
+        boton4=0;
+}
+})
+var boton5; 
+boton5=0;
+
+document.getElementById('Experiencialaboral1').addEventListener('click', function(){
+if(boton5==0){
+    document.getElementById('divexplabo').style.display="block";
+    boton5=1;
+}
+else{
+    document.getElementById('divexplabo').style.display="none";
+    boton5=0;
+}
+})
+var boton9; 
+boton9=0;
+
+document.getElementById('prueba').addEventListener('click', function(){
+boton9++; 
+document.getElementById('textoprueba').innerHTML=boton9;
+
+})*/
